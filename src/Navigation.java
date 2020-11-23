0d400f18ea61e0d968762a00dc80178f3b164a3b
oliverbskau <48017544+oliverbskau@users.noreply.github.com>
2020-11-23 12:45:20 +0100

    class navigation

---
+++ src/Navigation.java
@@ -0,0 +1,17 @@
+public class Navigation {
+
+    public static void menuNavigation(){
+
+        boolean navigator = true;
+
+        int choice = 0;
+        while(navigator){
+           switch(choice){
+
+
+
+
+           }
+        }
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,10 @@ public class Main {
 
 
     public static void main(String[] args) {
-
+        Main main = new Main();
+        main.run();
+    }
+    void run(){
 
     }
 }
