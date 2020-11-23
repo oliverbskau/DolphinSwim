33d4cf8ee94dfc928e0a2ea8161d476e3512a00c
oliverbskau <48017544+oliverbskau@users.noreply.github.com>
2020-11-23 12:52:31 +0100

    getters setters class Clubmember

---
+++ src/Clubmember.java
@@ -0,0 +1,35 @@
+public class Clubmember {
+
+    int age;
+    int ageType;
+    String name;
+    int memberType;
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAgeType() {
+        return ageType;
+    }
+    public void setAgeType(int ageType) {
+        this.ageType = ageType;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMemberType() {
+        return memberType;
+    }
+    public void setMemberType(int memberType) {
+        this.memberType = memberType;
+    }
+}
