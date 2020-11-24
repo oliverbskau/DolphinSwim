d35be498d950ee122e7f2aaec3f5eb73bcf7c4c2
jona881a <43369222+jona881a@users.noreply.github.com>
2020-11-24 11:22:21 +0100

    Lavet resources med filer der muligvis skal bruges i programmet og lavet en class Competitionswimmers der extender clubmember

---
+++ src/com/company/Competitionswimmers.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Competitionswimmers extends Clubmember{
+
+    public Competitionswimmers(int age, String ageType, String name, String memberType) {
+        super(age, ageType, name, memberType);
+    }
+
+
+
+}
