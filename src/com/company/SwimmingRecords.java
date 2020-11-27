609ab78a7fcc943d3c3f563a6366ec94a712d7c3
oliverbskau <48017544+oliverbskau@users.noreply.github.com>
2020-11-27 13:22:11 +0100

    abdi

--- src/com/company/SwimmingRecords.java
+++ src/com/company/SwimmingRecords.java
@@ -11,7 +11,8 @@ public class SwimmingRecords {
 
     public void registerBestSwimtime(ArrayList<Competitionswimmer> competitionswimmers) {
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("\nHvem skal der registreres tid på? ");
+        System.out.println("\nSkriv tallet foran navnet på den der skal ændres tid på");
+        System.out.print("Vælg: ");
         int choice = scanner.nextInt()-1;
 
         Competitionswimmer competitionswimmer = competitionswimmers.get(choice);
@@ -22,7 +23,8 @@ public class SwimmingRecords {
         scanner.nextLine();
 
         System.out.println("Hvad er tiden der skal registeres? (I sekunder)");
-        swimTime = scanner.nextLine() + ", " + today;
+        System.out.print("Tid: ");
+        swimTime = scanner.nextLine() + " Sekunder, " + today;
         System.out.printf("\nTid registreret: %s",swimTime);
         competitionswimmer.setPersonalBest(swimTime);
     }
@@ -33,7 +35,7 @@ public class SwimmingRecords {
 
         for(int i = 0; i < competitionswimmers.size(); i++) {
             String personalBest = competitionswimmers.get(i).getPersonalBest();
-            String time = personalBest.substring(0,personalBest.indexOf(','));
+            String time = personalBest.substring(0,personalBest.indexOf(" Sekunder,"));
             top5.add(Double.parseDouble(time));
         }
         Collections.sort(top5);
@@ -46,9 +48,11 @@ public class SwimmingRecords {
 
     public void printAll(ArrayList<Competitionswimmer> competitionswimmers){
         int competitionMember = 0;
+        System.out.println();
         for(int i = 0 ; i < competitionswimmers.size(); i++){
             competitionMember++;
             System.out.println(competitionMember + ". " + competitionswimmers.get(i).printSwimRecords());
+            System.out.println();
         }
     }
 
